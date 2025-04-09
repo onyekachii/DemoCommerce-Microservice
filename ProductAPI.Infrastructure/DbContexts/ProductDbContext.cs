a2e79fd3d9615180c586fccc0f2b10af4492c9c5
Kachii007 <ugwu_kennedy@yahoo.com>
2025-04-09 09:12:28 +0100

    Migration and db setup

--- ProductAPI.Infrastructure/DbContexts/ProductDbContext.cs
+++ ProductAPI.Infrastructure/DbContexts/ProductDbContext.cs
@@ -4,8 +4,15 @@ using ProductAPI.Domain.Entities;
 
 namespace ProductAPI.Infrastructure.DbContexts
 {
-    public class ProductDbContext(DbContextOptions<ProductDbContext> opt): DbContext
+    public class ProductDbContext(DbContextOptions<ProductDbContext> opt): DbContext(opt)
     {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
     }
 }
---
+++ ProductAPI.Infrastructure/Migrations/20250409064028_AddDecimalPrecisionToPrice.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDecimalPrecisionToPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
